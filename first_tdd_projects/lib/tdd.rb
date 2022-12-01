c84585af9ac383e0b57fa890550cb1dc7ca3f43a
“Emmett <emmett.wechsler11@gmail.com>
2022-12-01 15:22:30 -0500

    my_uniq

--- first_tdd_projects/lib/tdd.rb
+++ first_tdd_projects/lib/tdd.rb
@@ -1,3 +1,8 @@
-def my_uniq(array)
-    Array
+def my_uniq(arr)
+    ele_hash = Hash.new(0)
+    arr.each do |ele|
+        ele_hash[ele] = "?"
+    end
+
+    ele_hash.keys
 end
--- first_tdd_projects/spec/tdd_spec.rb
+++ first_tdd_projects/spec/tdd_spec.rb
@@ -3,16 +3,18 @@ require_relative "../lib/tdd.rb"
 describe "#my_uniq" do
 
     context "with valid argument" do
-
         it "should accept array as argument" do
-            arr = [1,2,3]
-            expect(my_uniq(arr)).to eq(Array)
+            expect { my_uniq([1,2,3]) }.to_not raise_error
         end
-        
     end
 
+    it 'should return an array of only unique elements' do
+        expect(my_uniq([1,1,1])).to eq([1])
+        expect(my_uniq(["a","b","c","a"])).to eq(["a","b","c"])
+    end
 
-    context "with invalid argument" do
-        it "should raise error if not given an array" 
+    it 'should return a new array' do
+        arr = [1,2,3]
+        expect(my_uniq(arr)).to_not be(arr)
     end
 end
